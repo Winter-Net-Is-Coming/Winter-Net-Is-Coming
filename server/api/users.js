1578a3842f158fa43556e00b33ca5304b2525bbb
bhavani kolla <bhavani.kolla@gmail.com>
2021-03-22 16:00:11 -0400

    added api route for posting a new user and deleting a user

--- server/api/users.js
+++ server/api/users.js
@@ -39,6 +39,73 @@ router.get('/:id', async (req, res, next) => {
   });
 
 
+//update single user details
+//  /api/users/id
+router.put('/:id', async(req, res,next) => {
+
+    try{
+      const id = req.params.id;
+      const user = await User.findByPk(id);
+      if(!user) {
+        res.sendStatus(404);
+        return;
+      }
+
+      const updateduser = await user.update(req.body);
+      res.status(200).send(updatedStudent);
+      
+    } catch (error) {
+        next(error);
+    }
+  });
+
+
+ // delete a user by id
+ // /api/users/id
+ router.delete('/:id', async(req, res, next) => {
+    try{
+      const id = req.params.id;
+      if(isNaN(Number(id))) {
+        res.sendStatus(400); 
+        return
+      }
+     
+      const userToBeDeleted = await User.findByPk(id);
+     if(userToBeDeleted === null){
+        res.sendStatus(404);
+        return;
+      } 
+       if(userToBeDeleted) {
+         await User.destroy({
+        where: {
+          id: id
+        }
+      });
+     res.sendStatus(204);
+     return;
+     } 
+    } catch (error) {
+      next(error);
+    }
+  });
+
+
+  //create a new user
+  // /api/users
+  router.post('/', async(req, res, next) => {
+    try {
+    const newUser = await User.create(req.body);
+    res.json(newUser)
+
+    } catch(error) {
+        next(error)
+    }
+    });
+
+  
+
+
+
 
   
 
