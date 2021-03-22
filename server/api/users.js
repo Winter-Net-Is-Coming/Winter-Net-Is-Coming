56683d53829e58f61ccb1adc6c5c82eea8dfd5ae
bhavani kolla <bhavani.kolla@gmail.com>
2021-03-22 15:11:20 -0400

    implemented api routes for getting all users and a single user

--- server/api/users.js
+++ server/api/users.js
@@ -1,3 +1,46 @@
+const router = require('express').Router();
+const {User} = require('../db/models');
+//const {ensureAdmin, ensureLogin} = require('./middleware')
+
+//all these routes are accessible only for admin users
+// get all users (only for admin) (/api/users)
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+    attributes: ['id', 'email', 'userName']
+    })
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
+});
+
+//get user by id(/api/users/id)
+router.get('/:id', async (req, res, next) => {
+    try {
+      const {id} = req.params
+  
+      if (isNaN(Number(id))) {
+        res.sendStatus(400)
+        return
+      }
+  
+      const singleUser = await User.findByPk(id)
+  
+      if (!singleUser) {
+        res.sendStatus(404)
+        return
+      }
+  
+      res.json(singleUser)
+    } catch (error) {
+      next(error)
+    }
+  });
+
+
+
+  
 
 
 
