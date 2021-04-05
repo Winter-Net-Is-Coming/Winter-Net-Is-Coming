4c1a5d31b5608f5685161e8bfb1a4df3556e7d7b
bhavani kolla <bhavani.kolla@gmail.com>
2021-04-05 13:02:56 -0400

    add score in api/index.js

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'));
+router.use('/score', require('./game-profile'));
 //router.use('/games', require('./games'));
 
 
@@ -9,4 +10,4 @@ router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
   next(error)
-})
+});
